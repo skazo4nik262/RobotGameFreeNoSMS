1b6df96d8702a212f9fdeec168bd3565b3e19fd8
Данил Тимофеев <danil2006timofeev@gmail.com>
2023-12-15 10:09:25 +0300

    New Full

--- Controller.cs
+++ Controller.cs
@@ -13,13 +13,11 @@ internal class Controller
 
     internal void StartListner()
     {
-        Console.WriteLine("2 - непреадолимое для самурая препятствие, 1 - возможность для достижения цели самурая , 3 - самурай, 4 - цель жизни самурая");
-        Console.WriteLine("Введите 25 символов для определения поля битвы самурая");
-        string firstCommands = Console.ReadLine();  //внесение данных о поле в одне строку 25 символов
+        string[] firstCommands = RandomGeneratingOfField.GetInstance().Random();  //внесение данных о поле в одне строку 25 символов
 
         int[] value = new int[25]; //массив куда поместится поле, которое станет двухмерным в будущем
         for(int i = 0; i < 25; i++)
-            value[i] = int.Parse(firstCommands[i].ToString());
+            value[i] = int.Parse(firstCommands[i]);
 
         mainCommander.Execute(value);   // первый запрос на 25 символов (рисование поля) передается mainCommander в метод Execute
 
---
+++ RandomGeneratingOfField.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+public class RandomGeneratingOfField
+{
+
+    private static RandomGeneratingOfField instance;  // тут организовать синглтон
+
+    public static RandomGeneratingOfField GetInstance()
+    {
+        if (instance == null)
+            instance = new RandomGeneratingOfField();
+        return instance;
+    }
+
+    Random random = new Random();
+    int samurai = 0;
+    int field = 0;
+    int walls = 0;
+    string[] result = new string[25];
+
+    public string[] Random()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            result[i] = random.Next(1, 5).ToString();
+
+            switch (result[i])
+            {
+                case "2":
+                    walls++;
+                    break;
+                case "3":
+                    samurai++;
+                    break;
+                case "4":
+                    field++;
+                    break;
+            }
+        }
+        return result;
+    }
+}
