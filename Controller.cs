f7bc79bb6cbbef2704d816b7a647daacc574e47d
Данил Тимофеев <danil2006timofeev@gmail.com>
2023-12-15 08:37:39 +0300

    Full

--- Controller.cs
+++ Controller.cs
@@ -2,6 +2,8 @@
 internal class Controller
 {
     private MainCommander mainCommander;
+    Robot robot = Robot.GetInstance();
+    Field field = Field.GetInstance();
 
     public Controller(MainCommander mainCommander)
     {
@@ -9,30 +11,21 @@ internal class Controller
         mainCommander.SetInterpretator(new FieldCommander());
     }
 
-    // цикл по запросу команды
     internal void StartListner()
     {
-        string firstCommands = "11111121121111211111111134";// Console.ReadLine();
-        int[] value = new int[25];
+        Console.WriteLine("2 - непреадолимое для самурая препятствие, 1 - возможность для достижения цели самурая , 3 - самурай, 4 - цель жизни самурая");
+        Console.WriteLine("Введите 25 символов для определения поля битвы самурая");
+        string firstCommands = Console.ReadLine();  //внесение данных о поле в одне строку 25 символов
+
+        int[] value = new int[25]; //массив куда поместится поле, которое станет двухмерным в будущем
         for(int i = 0; i < 25; i++)
-        {
             value[i] = int.Parse(firstCommands[i].ToString());
-        }
-        mainCommander.Execute(value);
-        mainCommander.SetInterpretator(new RobotCommander());
 
-        Robot robot = Robot.GetInstance();
-        Field field = Field.GetInstance();
+        mainCommander.Execute(value);   // первый запрос на 25 символов (рисование поля) передается mainCommander в метод Execute
 
-        while ((robot.X != field.X) || (robot.Y != field.Y))
-        {
+        mainCommander.SetInterpretator(new RobotCommander());  // смена интерпретатора у mainCommander на RobotCommander
+
+        while ((robot.X != field.X) || (robot.Y != field.Y)) // остальные запросы передаются туда же (цикл, пока робот не достигнет конечной точки)
             mainCommander.Execute(Console.ReadLine().Select(s=>int.Parse(s.ToString())).ToArray());
-        }
-
-
-
-        // первый запрос на 25 символов (рисование поля) передается mainCommander в метод Execute
-        // смена интерпретатора у mainCommander на RobotCommander
-        // остальные запросы передаются туда же (цикл, пока робот не достигнет конечной точки)
     }
 }
